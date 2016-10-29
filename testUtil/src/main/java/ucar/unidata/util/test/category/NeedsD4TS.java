f19821406b673590276d22ef0ff9dc9bbcdbf0b6
Dennis Heimbigner <dmh@ucar.edu>
2016-10-28 20:21:14 -0600

    ckp

---
+++ testUtil/src/main/java/ucar/unidata/util/test/category/NeedsD4TS.java
@@ -0,0 +1,13 @@
+package ucar.unidata.util.test.category;
+
+/**
+ * A marker to be used with JUnit categories to indicate that a test method or test class
+ * cannnot be run without access to te d4ts server.
+ * Using this category allows easy searching for such test cases.
+ *
+ * @author dmh
+ * @since 2016/10/21
+ */
+public interface NeedsD4TS
+{
+}
